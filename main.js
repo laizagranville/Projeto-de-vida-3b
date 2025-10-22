ab472d3b3ce950f338248b484e6f1bb83e664f2e
laíza <164918287+laizagranville@users.noreply.github.com>
2025-10-22 14:50:02 -0300

    jmj

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ botoes[j].classList.remove("ativo");
 textos[j].classList.remove("ativo");
 }
 botoes[i].classList.add("ativo");
-textos[j].classList.add("ativo");
+textos[i].classList.add("ativo");
 }
 }
