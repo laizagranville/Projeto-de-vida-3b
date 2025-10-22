ea6b863b887d3a3b32f24d239e854b628b4d50ed
laíza <164918287+laizagranville@users.noreply.github.com>
2025-10-22 15:04:10 -0300

    kfyuk

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ botoes[i].classList.add("ativo");
 textos[i].classList.add("ativo");
 }
 }
+  const tempoOnjetivo1 = const name = new Date("2023-10-05T00:00:00");
+  const tempoOnjetivo2 = new Date("2023-12-05T00:00.00");
+  const tempoOnjetivo3 = new Date("2023-12-01T00:00.00");
+  const tempoOnjetivo4 = new Date("2024-02-01T00:00.00");
+  const tempos = [tempoOnjetivo1, tempoOnjetivo2, tempoOnjetivo3, tempoOnjetivo4];
