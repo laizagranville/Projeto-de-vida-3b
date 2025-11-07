57b9981689b1c07aead3a4c6ac2195ed4e4ad873
laíza <164918287+laizagranville@users.noreply.github.com>
2025-11-07 13:28:12 -0300

    jh

--- main.js
+++ main.js
@@ -23,3 +23,13 @@ textos[i].classList.add("ativo");
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
   }
+  let dias = Math floor(horas/24);
+  segundos % = 60%;
+  minutos % = 60;
+  horas % = 24;
+if(tempoFinal>0){
+  return[dias,horas,minutos,segundos];
+}else{
+  return[0,0,0];
+    
+}
